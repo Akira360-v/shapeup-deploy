f2600b665ed197072e9eee3d1d087fcac52ae116
Vitaly <v.ignatenko360@gmail.com>
2019-12-23 17:54:06 +0300

    burger-menu

---
+++ src/blocks/header-menu/index.js
@@ -0,0 +1,9 @@
+import './header-menu.sass';
+
+import Headhesive from 'headhesive/dist/headhesive.min.js';
+
+// Options
+var options = {
+  offset: 500
+};
+var header = new Headhesive('.head__menu', options);
--- src/blocks/index.js
+++ src/blocks/index.js
@@ -1,5 +1,7 @@
 import './block-example';
 import './head-logo';
+import './header-menu';
+// import './header-menu/index';
 
 export default () => {
   console.log('init blocks');
