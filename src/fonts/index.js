0613cbc917788c7afbb3278e8298ab909ef69a50
v.ignatenko360 <v.ignatenko360@gmail.com>
2019-12-30 17:56:34 +0300

    rt4 +-

--- src/fonts/index.js
+++ src/fonts/index.js
@@ -1,1 +1,2 @@
 import './fonts.sass';
+import './swiper.min.css';
--- src/layout/about/index.js
+++ src/layout/about/index.js
@@ -1,1 +1,27 @@
 import './about.sass';
+
+import Swiper from 'swiper/js/swiper.min.js';
+// import Swiper from 'swiper/css/swiper.min.css';
+
+
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  loop: true,
+  freeMode: true,
+  loopedSlides: 5, //looped slides should be the same
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+});
+var galleryTop = new Swiper('.gallery-top', {
+  spaceBetween: 10,
+  loop:true,
+  loopedSlides: 5, //looped slides should be the same
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  thumbs: {
+    swiper: galleryThumbs,
+  },
+});
