8eb7d72c30074d910cbb017f941f1be987b841a3
Vitaly <v.ignatenko360@gmail.com>
2020-01-09 15:31:45 +0300

    rt4 mobill+

--- src/layout/about/index.js
+++ src/layout/about/index.js
@@ -1,26 +1,26 @@
 import './about.sass';
-
 import Swiper from 'swiper/js/swiper.min.js';
-// import Swiper from 'swiper/css/swiper.min.css';
-
 
 var galleryThumbs = new Swiper('.gallery-thumbs', {
-  spaceBetween: 30,
+  spaceBetween: 15,
   slidesPerView: 3,
   loop: true,
   freeMode: true,
   loopedSlides: 5, //looped slides should be the same
   watchSlidesVisibility: true,
   watchSlidesProgress: true,
+  breakpoints: {
+      600: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      }
+    },
 });
+
 var galleryTop = new Swiper('.gallery-top', {
   spaceBetween:   25,
   loop:true,
   loopedSlides: 5, //looped slides should be the same
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
   thumbs: {
     swiper: galleryThumbs,
   },
